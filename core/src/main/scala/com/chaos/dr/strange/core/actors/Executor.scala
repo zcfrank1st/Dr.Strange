1eea7fe1c7da91ceffe53cbb685fd16a286f7a03
zcfrank1st <zcfrank1st@sina.com>
2017-02-08 12:22:49 +0800

    change code structure

--- core/src/main/scala/com/chaos/dr/strange/core/actors/Executor.scala
+++ core/src/main/scala/com/chaos/dr/strange/core/actors/Executor.scala
@@ -1,13 +1,10 @@
 package com.chaos.dr.strange.core.actors
 
 import akka.actor.Actor
-import akka.actor.Actor.Receive
-import com.chaos.dr.strange.meta.actor
 
 /**
   * Created by zcfrank1st on 08/02/2017.
   */
-@actor(0)
 class Executor extends Actor {
   override def receive: Receive = ???
 }
--- macros/src/main/scala/com/chaos/dr/strange/meta/application.scala
+++ macros/src/main/scala/com/chaos/dr/strange/meta/application.scala
@@ -23,20 +23,3 @@ class application(entrance: String) extends StaticAnnotation {
     q"object $name extends App { $main }"
   }
 }
-
-class actor(typ: Int) extends StaticAnnotation {
-
-  inline def apply(defn: Any): Any = meta {
-    val arg = this match {
-      case q"new $_(${Lit(typ: Int)})" => typ
-      case _                                                => // nothing to do
-    }
-
-    val q"class $name extends Actor { ..$body }" = defn
-
-    arg match {
-      case 0 => defn
-      case 1 => q"class $name extends PersistentActor { ..$body }"
-    }
-  }
-}
