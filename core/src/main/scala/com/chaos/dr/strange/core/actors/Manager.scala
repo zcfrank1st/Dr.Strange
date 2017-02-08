d90a313ffaf0b12f8ac33a3e84773769af08a0c4
zcfrank1st <zcfrank1st@sina.com>
2017-02-08 17:42:01 +0800

    change meta

--- core/src/main/scala/com/chaos/dr/strange/core/actors/Manager.scala
+++ core/src/main/scala/com/chaos/dr/strange/core/actors/Manager.scala
@@ -1,15 +1,15 @@
 package com.chaos.dr.strange.core.actors
 
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
 
 /**
   * Created by zcfrank1st on 08/02/2017.
   */
-class Manager {
-
-}
-
-object Manager {
-  def p() = {
-    println("haha __ ads ")
+class Manager extends Actor {
+  override def receive: Receive = {
+    case 0 => println("hello world")
+    case _ => // nothing to do
   }
 }
--- macros/src/main/scala/com/chaos/dr/strange/meta/application.scala
+++ macros/src/main/scala/com/chaos/dr/strange/meta/application.scala
@@ -18,15 +18,29 @@ class application(entrance: String) extends StaticAnnotation {
 
     val q"object $name" = defn
 
-
     val imports = s"import $arg".parse[Stat].get
+
+    val argList = arg.split("\\.")
+    val typString = argList(argList.size - 1)
+    val typ = typString.parse[Type].get
 
     val main =
       q"""
-        println("hello world  1231231")
-        println("666")
-        $imports
-        Manager.p
+         $imports
+         import akka.actor._
+
+         class Terminator(ref: ActorRef) extends Actor with ActorLogging {
+             context watch ref
+             def receive = {
+               case Terminated(_) =>
+                 log.info("{} has terminated, shutting down system", ref.path)
+                 context.system.terminate()
+             }
+         }
+
+         val system = ActorSystem("DrStrange")
+         val a = system.actorOf(Props[$typ], "manager")
+         system.actorOf(Props(classOf[Terminator], a), "terminator")
        """
 
 
