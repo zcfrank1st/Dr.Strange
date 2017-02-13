e870b2d7e9da4b24f1074413e9de88a73d6acbb0
zcfrank1st <zcfrank1st@sina.com>
2017-02-13 15:12:49 +0800

    add stores

--- core/src/main/scala/com/chaos/dr/strange/core/actors/store/MysqlStore.scala
+++ core/src/main/scala/com/chaos/dr/strange/core/actors/store/MysqlStore.scala
@@ -5,7 +5,9 @@ import com.chaos.dr.strange.core.models.Task
   * Created by zcfrank1st on 13/02/2017.
   */
 trait MysqlStore extends Store {
-  override def keep(task: Task): Unit = ???
+  override def keep(task: Task): Unit = {
+    // TODO 保存数据库
+  }
 }
 
 object MysqlStore
