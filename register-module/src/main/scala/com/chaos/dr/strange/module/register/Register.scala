208ccb271b253e78cb9e671f01f42ea234601691
zcfrank1st <zcfrank1st@sina.com>
2017-02-16 14:55:13 +0800

    add register module

---
+++ register-module/src/main/scala/com/chaos/dr/strange/module/register/Register.scala
@@ -0,0 +1,11 @@
+package com.chaos.dr.strange.module.register
+
+/**
+  * Created by zcfrank1st on 16/02/2017.
+  */
+trait Register {
+
+  object RedisRegister {
+    //TODO use redis to register and unregister
+  }
+}
