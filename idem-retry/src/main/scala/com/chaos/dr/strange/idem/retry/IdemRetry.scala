c0d3993c06da9082348a821ce459e6a04da08f87
zcfrank1st <zcfrank1st@sina.com>
2017-02-17 09:56:08 +0800

    add non idem retry

--- idem-retry/src/main/scala/com/chaos/dr/strange/idem/retry/IdemRetry.scala
+++ idem-retry/src/main/scala/com/chaos/dr/strange/idem/retry/IdemRetry.scala
@@ -1,4 +1,4 @@
-package com.chaos.dr.strange.retry
+package com.chaos.dr.strange.idem.retry
 
 import akka.actor.{ActorSystem, Props}
 import com.chaos.dr.strange.module.executor.RetryExecutor
@@ -7,12 +7,11 @@ import com.chaos.dr.strange.module.persistence.Persistence
 /**
   * Created by zcfrank1st on 16/02/2017.
   */
-object Retry extends App with Persistence {
+object IdemRetry extends App with Persistence {
   val system = ActorSystem("retry")
 
   import system.dispatcher
   import scala.concurrent.duration._
 
-  // FIXME 不幂等情况下多次执行如何规避 （数据库增加幂等字段标明，幂等只执行一次）
   system.scheduler.schedule(0 seconds, 5 minutes, system.actorOf(Props[RetryExecutor]), MysqlPersistence.retrieveIdemFails())
 }
---
+++ non-idem-retry/src/main/scala/com/chaos/dr/strange/non/idem/retry/NonIdemRetry.scala
@@ -0,0 +1,8 @@
+package com.chaos.dr.strange.non.idem.retry
+
+/**
+  * Created by zcfrank1st on 17/02/2017.
+  */
+object NonIdemRetry extends App {
+  // TODO 不幂等情况下执行
+}
